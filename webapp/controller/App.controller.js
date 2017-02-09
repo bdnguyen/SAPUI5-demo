da303ee0412c25d908a7610e5ac44165d83f51ba
Daniel BD Nguyen <danielbdnguyen@gmail.com>
2017-02-09 08:48:43 +0000

    Data-binding showcase, bind view's input field value to the model in the controller

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,14 +1,29 @@
 sap.ui.define(
-	["sap/ui/core/mvc/Controller",
-		"sap/m/MessageToast"
+	[
+		"sap/ui/core/mvc/Controller",
+		"sap/m/MessageToast",
+		"sap/ui/model/json/JSONModel"
 	],
-	
-	function(Controller, MessageToast) {
+
+	function(Controller, MessageToast, JSONModel) {
 		"use strict";
 		return Controller.extend("sap.ui.demo.wt.controller.App", {
+
+			onInit: function() {
+				// set data model on view
+				var oData = {
+					recipient: {
+						name: "Universe"
+					}
+				};
+				var oModel = new JSONModel(oData);
+				this.getView().setModel(oModel);
+			},
+
 			onShowHello: function() {
 				MessageToast.show("Hello World");
 			}
+			
 		});
 	}
 );
