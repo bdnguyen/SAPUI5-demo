b209d9e223a2a7280b47c22aec071294b2ff7bca
Daniel BD Nguyen <danielbdnguyen@gmail.com>
2017-02-09 11:00:32 +0000

    Replace rootView with manifest json (ref to the descriptor will be loaded automatically when component is instantiated). Remove ResourceModel dependancy

--- webapp/Component.js
+++ webapp/Component.js
@@ -4,11 +4,11 @@ sap.ui.define([
 	"sap/ui/model/resource/ResourceModel"
 	], 
 	
-	function (UIComponent, JSONModel, ResourceModel) {
+	function (UIComponent, JSONModel) {
 		"use strict";
 		return UIComponent.extend("sap.ui.demo.wt.Component", {
 			metadata : {
-				rootView: "sap.ui.demo.wt.view.App"
+				manifest: "json"
 			},
 			
 			init : function () {
@@ -23,11 +23,6 @@ sap.ui.define([
 				var oModel = new JSONModel(oData);
 				this.setModel(oModel);
 				
-				// set i18n model
-				var i18nModel = new ResourceModel({
-					bundleName : "sap.ui.demo.wt.i18n.i18n"
-				});
-				this.setModel(i18nModel, "i18n");
 			}
 		});
 	}
