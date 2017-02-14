2c7ec8a1243fd96d5bc2c9fb371db7157f95ab2d
Daniel BD Nguyen <danielbdnguyen@gmail.com>
2017-02-14 10:40:04 +0000

    Add onOpenDialog also to the app controller so dialog will open with a ref to the current view

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -7,6 +7,9 @@ sap.ui.define(
 	function(Controller) {
 		"use strict";
 		return Controller.extend("sap.ui.demo.wt.controller.App", {
+			onOpenDialog : function () {
+				this.getOwnerComponent().openHelloDialog();
+			}
 		});
 	}
 	
