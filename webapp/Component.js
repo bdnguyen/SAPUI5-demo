2be257510eb1b6f37faa2ddb6496ebcd7299a3d5
Daniel BD Nguyen <danielbdnguyen@gmail.com>
2017-02-14 10:21:27 +0000

    Dialog instantiation refactored to a new helper object

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,10 +1,10 @@
 sap.ui.define([
 	"sap/ui/core/UIComponent",
 	"sap/ui/model/json/JSONModel",
-	"sap/ui/model/resource/ResourceModel"
+	"sap/ui/demo/wt/controller/HelloDialog"
 	], 
 	
-	function (UIComponent, JSONModel) {
+	function (UIComponent, JSONModel, HelloDialog) {
 		"use strict";
 		return UIComponent.extend("sap.ui.demo.wt.Component", {
 			metadata : {
@@ -23,6 +23,12 @@ sap.ui.define([
 				var oModel = new JSONModel(oData);
 				this.setModel(oModel);
 				
+				// set dialog
+				this._helloDialog = new HelloDialog(this.getAggregation("rootControl"));
+			},
+			
+			openHelloDialog : function () {
+				this._helloDialog.open();
 			}
 		});
 	}
